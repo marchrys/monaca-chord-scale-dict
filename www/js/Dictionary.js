5fb6ac988400dea8e62cc1b2c439c44752f7bf94
marchrys <mar.chrysikopoulos@gmail.com>
2021-05-23 17:54:58 +0900

    Added the tonic select for both dictionaries.

--- www/js/Dictionary.js
+++ www/js/Dictionary.js
@@ -1,6 +1,21 @@
 class Dictionary {
   constructor(data, lang) {
-    console.log('constructor!');
+    this.containerDiv = document.getElementById(data.containerDiv);
+    
+    this.containerDiv.innerHTML += 
+        '<div class="input-field col s12">' +
+          '<select class="tonic-select"></select>' +
+          '<label>tonique</label>' +
+        '</div>';
+
+    this.tonicSelect = this.containerDiv.querySelector('.tonic-select');
+
+    this.initSelects();
+  }
+
+  initSelects() {
+    const elems = document.querySelectorAll('select');
+    const instances = M.FormSelect.init(elems, {});
   }
 }
 
