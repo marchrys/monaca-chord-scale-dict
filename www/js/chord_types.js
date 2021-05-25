8b59a63a3864ae5232d2f213c5a686160647c597
marchrys <mar.chrysikopoulos@gmail.com>
2021-05-26 02:40:52 +0900

    Ajout de l'array de types d'accords et de gammes.

---
+++ www/js/chord_types.js
@@ -0,0 +1,34 @@
+const chordTypes = [
+  {
+    id: 1,
+    name: {
+      en: '(major)',
+      fr: '(majeur)'
+    },
+    intervals: [10,18]
+  },
+  {
+    id: 2,
+    name: {
+      en: 'm',
+      fr: 'm'
+    },
+    intervals: [8,18]
+  },
+  {
+    id: 3,
+    name: {
+      en: 'dim',
+      fr: 'dim'
+    },
+    intervals: [8,16]
+  },
+  {
+    id: 4,
+    name: {
+      en: 'aug',
+      fr: 'aug'
+    },
+    intervals: [10,20]
+  },
+];
