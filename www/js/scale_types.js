b0af3d2de595dc1cd6e5bc13f941a1b35f9e846a
marchrys <mar.chrysikopoulos@gmail.com>
2021-05-28 16:26:42 +0900

    Ajout d\'autres types de gamme.

--- www/js/scale_types.js
+++ www/js/scale_types.js
@@ -23,5 +23,61 @@ const scaleTypes = [
     },
     intervals: [5, 8, 13, 18, 21, 26]
   },
+  {
+    id: 4,
+    name: {
+      en: 'ionian mode',
+      fr: 'mode de do (ionien)'
+    },
+    intervals: [5, 10, 13, 18, 23, 28]
+  },
+  {
+    id: 5,
+    name: {
+      en: 'dorian mode',
+      fr: 'mode de ré (ionien)'
+    },
+    intervals: [5, 8, 13, 18, 23, 26]
+  },
+  {
+    id: 6,
+    name: {
+      en: 'phrygian mode',
+      fr: 'mode de mi (phrygien)'
+    },
+    intervals: [3, 8, 13, 18, 21, 26]
+  },
+  {
+    id: 7,
+    name: {
+      en: 'lydian mode',
+      fr: 'mode de fa (lydien)'
+    },
+    intervals: [5, 10, 15, 18, 23, 28]
+  },
+  {
+    id: 8,
+    name: {
+      en: 'mixolydian mode',
+      fr: 'mode de sol (mixolydien)'
+    },
+    intervals: [5, 10, 13, 18, 23, 26]
+  },
+  {
+    id: 9,
+    name: {
+      en: 'aeolian mode',
+      fr: 'mode de la (aéolien)'
+    },
+    intervals: [5, 8, 13, 18, 21, 26]
+  },
+  {
+    id: 10,
+    name: {
+      en: 'locrian mode',
+      fr: 'mode de si (locrien)'
+    },
+    intervals: [3, 8, 13, 16, 23, 26]
+  },
 ];
 
